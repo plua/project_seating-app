979ebff82d2b7b79ade5dea2f90d24ee934e64ef
Pam Luangrath <pam.luangrath@gmail.com>
2015-09-05 02:36:15 -0400

    Initial JS, setting up steps for JS code

--- js/script.js
+++ js/script.js
@@ -10,3 +10,21 @@ document.getElementById("assign").addEventListener('click', reserveSeat)
 function reserveSeat(){
 
 }
+
+/* Step 1: Generate HTML */
+
+ function Theatre(name,numberOfSeats,id) {
+ 	this.name = name;
+ 	this.numberOfSeats = numberOfSeats;
+ 	this.id = id;
+ }
+
+/* Step 2: When a div is clicked, have form pop up */
+
+
+
+/* Step 3: Let user submit information on the form */
+
+
+
+/* Step 4: Save user's information and associate it with the seat */
