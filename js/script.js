7090662e5a22c3a0f783589f26510a5914e1859e
Pam Luangrath <pam.luangrath@gmail.com>
2015-09-07 12:00:35 -0400

    Seat color changes once you submit form

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
-
-
-
 /* Step 1: Generate HTML */
-
 var wrapper = document.getElementById('wrapper');
-
+var form = document.getElementById('form');;
 var theatre = {
 	name: 'Grand Circus Theatre',
 	numberOfSeats: 24
 }
-
-
 function createSeats(){
  for (var i = 1; i < theatre.numberOfSeats+1; i++){
   var element = document.createElement('div');
@@ -21,13 +15,11 @@ function createSeats(){
    wrapper.appendChild(element);
  }
 };
-
+createSeats();
 
 /* Step 2: When a div is clicked, have form pop up */
 
-
 function showForm(id){
-	var form = document.getElementById('form');
 	var picked = document.getElementById('picked_seat');
 	form.style.display = "block";
 	var currentSeat = document.getElementById('current_seat');
@@ -35,49 +27,28 @@ function showForm(id){
 	picked.innerText = "You have chosen to reserve seat " + this.id.substr(5,6);
 };
 
-/*
-function showForm(id){
-	var form = document.getElementById('form');
-	form.innerHTML = '<h2>Seat Reservation Form</h2><p>You have chosen to reserve seat '+
-	 this.id.substr(5,6) +'</p><form>Name: <input class="'+ id +'"></input><br>Email: <input class="'+ 
-	 id +'"></input><br><div class="center"><button id="formButton">Reserve</button></div></form>';
-
-};
-*/
-createSeats();
-
-
 /* Step 3: Let user submit information on the form */
 
-
-
+var formButton = document.getElementById('formButton');
+formButton.addEventListener('click', getFormData);
+function getFormData() {
+	var formName = document.getElementById('form_name');
+	var formEmail = document.getElementById('form_email');
+	var formSeat = document.getElementById('current_seat');
+	var reservationName = formName.value;
+	var reservationEmail = formEmail.value;
+	var reservationSeat = formSeat.value;
+	document.getElementById(reservationSeat).className="unavailable";
+	appendFormData(reservationName, reservationEmail, reservationSeat);
+}
+function appendFormData(name,email,seat){
+	var reservationInfo = document.createElement('div');
+	form.style.display = "none";
+	reservationInfo.id = seat;
+	reservationInfo.className= 'unavailable';
+   	reservationInfo.innerHTML = name, email;
+   	reservationInfo.appendChild(element);	
+}
 
 /* Step 4: Save user's information and associate it with the seat */
 
-
-
-
-//Please keep the code below, I may build upon it or scrap it. -Pam
-
-/*
-//Created a class named Theatre and passed name, number of seats, and seat id through its parameter
- function Theatre(name,numberOfSeats,id) {
- 	this.name = name;
- 	this.numberOfSeats = numberOfSeats;
- 	this.id = id;
- }
-
-//We are adding a method getNumOfSeats to the prototype of Theatre
-//prototype is used so that javascript knows that everytime we create an instance of Theatre, it has a method called getNumOfSeats.
-//The method returns the number of seats passed through Theatre. This refers to our theatre object. 
-Theatre.prototype.getNumOfSeats = function(){
-	return this.numberOfSeats;
-};
-
-//Created a class name Seat which we'll pass in the seatNumber through its parameters
-//We are taking the id of Seat and setting it equal to what will become the id of our seat divs
-function Seat(seatNumber){
-	this.seatNumber = seatNumber;
-	this.id = "seat_" + this.seatNumber;
-}
-*/
